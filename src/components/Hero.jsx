152a50dac2371febdc37be372b6e276b4383668a
shripal <shripalp@gmail.com>
2023-09-30 12:27:34 -0600

    Hero section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,9 +15,13 @@ const Hero = () => {
         </button>
       </nav>
       <h1 className="head_text">
-        Summarize Articles with <br />
+        Summarize Articles with <br className="max-md:hidden" />
         <span className="orange_gradient">OpenAI GPT-4</span>
       </h1>
+      <h2 className="desc">
+        Simplyfy your readings with Summize, an open source article summarizer
+        that transforms lenghty articles into clear and concise summaries
+      </h2>
     </header>
   );
 };
