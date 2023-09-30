e1d7908434835683809bb18bd5842616da4aed6d
shripal <shripalp@gmail.com>
2023-09-30 13:19:14 -0600

    input field completed

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,12 +4,19 @@ import { useState, useEffect } from "react";
 import { linkIcon, copy, logo, tick } from "../assets/index.js";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+  const handleSubmit = async (e) => {
+    alert("Submitted");
+  };
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
@@ -19,8 +26,13 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Enter a url"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => {
+              setArticle({
+                ...article,
+                url: e.target.value,
+              });
+            }}
             required
             className="url_input peer"
           />
